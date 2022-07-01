70ec6ec3db0bca3285c965fc247578744e157048
Norapat Buppodom <new17353@gmail.com>
2022-07-02 00:43:22 +0700

    experiment: remove checksum model

--- easyocr/easyocr.py
+++ easyocr/easyocr.py
@@ -187,7 +187,8 @@ class Reader(object):
                     download_and_unzip(model['url'], model['filename'], self.model_storage_directory, verbose)
                     assert calculate_md5(model_path) == model['md5sum'], corrupt_msg
                     LOGGER.info('Download complete.')
-                elif calculate_md5(model_path) != model['md5sum']:
+                # elif calculate_md5(model_path) != model['md5sum']:
+                elif True:
                     if not self.download_enabled:
                         raise FileNotFoundError("MD5 mismatch for %s and downloads disabled" % model_path)
                     LOGGER.warning(corrupt_msg)
